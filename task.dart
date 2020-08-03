a3542b8392fcc94d237a88c0f39428da26b99880
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-03 23:08:02 +0900

    Create task.dart

---
+++ task.dart
@@ -0,0 +1,9 @@
+class Task {
+  int id;
+  String status;
+  String title;
+  String addedDate;
+  String completedDate;
+
+  Task({this.id, this.title, this.status, this.addedDate, this.completedDate});
+}
