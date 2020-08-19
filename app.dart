f2adf2ee9a00c01003cf6f9a93a3d2be4a6478c4
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-19 23:47:36 +0900

    Create app.dart

---
+++ app.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,//テーマ
+        accentColor: Colors.amberAccent,//アクションの色
+        buttonTheme: ButtonThemeData(//ボタンの角丸
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(25)),
+          )
+        )
+      ),
+      darkTheme: ThemeData(//ダークモード
+        brightness: Brightness.dark
+      ),
+      home: Provider<TodoBloc>(
+        create: (context) => TodoBloc(),
+        dispose: (context, bloc) => bloc.dispose(),
+        child:TodoListView()
+      ),
+    );
+  }
+}
+
+class TodoListView extends StatelessWidget {
+
+}
+
