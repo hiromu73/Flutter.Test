6d015748cab311ba7b628cc67e2b5238b8abbca2
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-22 22:13:12 +0900

    Create task_item.dart

---
+++ screen/task_screen/task_item.dart
@@ -0,0 +1,68 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:to_do_app/model/task.dart';
+
+class TaskItem extends StatelessWidget {
+  final Task task;
+  final VoidCallback onTap;
+  final Function(bool) toggleDone;
+
+  const TaskItem(
+      {Key key,
+        @required this.onTap,
+        @required this.task,
+        @required this.toggleDone})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0).copyWith(left: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    task.name,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  (task.memo.isEmpty)
+                      ? Container()
+                      : Column(
+                    children: <Widget>[
+                      SizedBox(height: 4),
+                      Text(
+                        task.memo,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.body1,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Checkbox(
+              value: task.isDone,
+              onChanged: (value) {
+                print(value);
+                toggleDone(value);
+              },
+              activeColor: Colors.lightGreen,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
