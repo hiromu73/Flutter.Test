9ad83d799df1b6dd9b4c1805ad8ec3b686c15642
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-22 21:52:59 +0900

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,34 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_app/screen/add_task_screen/add_task_screen.dart';
+import 'package:to_do_app/screen/task_screen/task_screen.dart';
+import 'package:to_do_app/view_model/task_view_model.dart';
 
-import 'components/app.dart';
 
 void main() {
-  runApp(TodoApp()); //TodoApp()を走らせて置くだけapp.dartへ
+  print(' Test Todo App !!!');
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TaskViewModel(),
+      child: MyApp(),
+    ),
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: TaskScreen.id,
+      routes: {
+        TaskScreen.id: (context) => TaskScreen(),
+        AddTaskScreen.id: (context) => AddTaskScreen(),
+      },
+    );
+  }
 }
