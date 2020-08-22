50817e7af451cb94975b84e801a039a910515f53
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-22 22:14:28 +0900

    Create task_screen.dart

---
+++ screen/task_screen/task_screen.dart
@@ -0,0 +1,31 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:to_do_app/screen/add_task_screen/add_task_screen.dart';
+import 'package:to_do_app/screen/task_screen/task_list_view.dart';
+
+class TaskScreen extends StatelessWidget {
+  static String id = 'task_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task List'),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, AddTaskScreen.id);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.add),
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        child: TaskListView(),
+      ),
+    );
+  }
+}
