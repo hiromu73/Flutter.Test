6f1cde3b3c50b581c8db3e3e72085025c2a532c8
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-22 22:08:04 +0900

    Create task.dart

---
+++ model/task.dart
@@ -0,0 +1,14 @@
+class Task {
+  String name;
+  String memo;
+  bool isDone;
+  final DateTime createdAt;
+  DateTime updatedAt;
+
+
+  Task({this.name,
+    this.memo,
+    this.isDone = false,
+    this.createdAt,
+    this.updatedAt});
+}
