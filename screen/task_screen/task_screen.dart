3036fb2cddd943b337d7077c39724ac3bbe39af3
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-24 00:24:31 +0900

    Update task_screen.dart

--- screen/task_screen/task_screen.dart
+++ screen/task_screen/task_screen.dart
@@ -4,19 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:to_do_app/screen/add_task_screen/add_task_screen.dart';
 import 'package:to_do_app/screen/task_screen/task_list_view.dart';
 
+
 class TaskScreen extends StatelessWidget {
   static String id = 'task_screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Task List'),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, AddTaskScreen.id);
+        title: Text('Todo List'),
+        actions: [//アクションボタン作成
+          GestureDetector(//タッチ/ボタン入力検出用途で利用
+            onTap: () {//シングルタップ時に呼ばれる
+              Navigator.pushNamed(context, AddTaskScreen.id);//ページを遷移させる
             },
-            child: Padding(
+            child: Padding(//タッチ検出対象
               padding: const EdgeInsets.all(8.0),
               child: Icon(Icons.add),
             ),
