2420f380bf397d0642a974ee31d2ccea75d85c37
ひろむ <61053388+hiromu73@users.noreply.github.com>
2020-08-31 00:27:27 +0900

    Update task.dart

--- model/task.dart
+++ model/task.dart
@@ -1,3 +1,27 @@
+
+
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+
+final Future<Database> database = openDatabase(//データベースに接続する
+  join(await getDatabasesPath(),'task_database.db'),//データベースファイルを保存するパスを取得する
+  onCreate: (db,version) {//SQLiteのテーブルを作成する.初回起動時に必ず実行
+    return db.execute(
+    "CREATE TABLE task(name TEXT , memo TEXT)",//Taskテーブルを作成
+    );
+  },
+  version: 1,
+  onUpgrade: (Database db, int oldVersion, int newVersion) async {
+    await db.execute("ALTER TABLE task ADD COLUMN create_at TIMESTAMP DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime'))");
+  },
+  );
+
+
+
+//データモデルを定義
 class Task {
   String name;
   String memo;
@@ -11,4 +35,31 @@ class Task {
     this.isDone = false,
     this.createdAt,
     this.updatedAt});
+
+  Map<String,dynamic> onPressed() {
+    return {
+      'name':name,
+      'memo':memo,
+      'isDone':isDone,
+    };
+  }
 }
+
+Future<void>insertTask(Task task) async {
+  final Database db = await database;
+  await db.insert(
+    'memo',
+    task.onPressed(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+final todo = Task(
+  name: 'テスト',
+  memo: 'テスト',
+);
+
+await insertTask(todo);
+
+final List<Map<String,dynamic>>maps = await db.query('memo');
+
